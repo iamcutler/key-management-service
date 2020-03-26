8b383c3ece62ad6f6186e5b152e724dc5439b9f2
Allan Cutler <allan.cutler@automationanywhere.com>
2020-03-25 22:51:27 -0700

    Introduced nestjs config to manage environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { jsonResponse } from './middleware/jsonresponse.middleware';
-
+// controllers
 import HealthController from './interfaces/health/health.controller';
-
+// modules
 import AuthModule from './domain/authentication/auth.module';
 import KeyManagementModule from './domain/key-management/key-management.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     AuthModule,
     KeyManagementModule,
   ],
